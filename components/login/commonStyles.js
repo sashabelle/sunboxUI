12e8110716491cf0ecf0b35975dce547ca56f9ed
Lycah Dinglasan <lycah.dinglasan@gmail.com>
2025-03-10 20:27:27 +0800

    Create commonStyles.js

---
+++ components/login/commonStyles.js
@@ -0,0 +1,21 @@
+export const COLORS = {
+  primary: "#CBB7B7",
+  accent: "#EEBD62",
+  dark: "#1E1E1E",
+  white: "#FFFFFF",
+};
+
+export const FONTS = {
+  regular: "Instrument Sans",
+  medium: "Instrument Sans",
+  semiBold: "Instrument Sans",
+  bold: "Instrument Sans",
+};
+
+export const SIZES = {
+  base: 8,
+  small: 12,
+  medium: 14,
+  large: 16,
+  xlarge: 31,
+};
