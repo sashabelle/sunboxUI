1518f273bedcb9c7deed8ae2cd426616e0c994de
Lycah Dinglasan <lycah.dinglasan@gmail.com>
2025-03-10 20:36:31 +0800

    Create styles.js

---
+++ components/login/styles.js
@@ -0,0 +1,12 @@
+export const colors = {
+  primary: "#CBB7B7",
+  accent: "#EEBD62",
+  dark: "#1E1E1E",
+};
+
+export const fonts = {
+  regular: "Instrument Sans",
+  medium: "Instrument Sans",
+  semiBold: "Instrument Sans",
+  bold: "Instrument Sans",
+};
